6378be8d926ea7b778214d0d235ffe8f8cc10030
BarackAdida <adida.barack@student.moringaschool.com>
2025-09-07 20:35:30 +0300

    Log in route added

--- the-farm/server/app.py
+++ the-farm/server/app.py
@@ -13,41 +13,55 @@ def create_app():
     bcrypt.init_app(app)
     migrate.init_app(app, db)
 
-    @app.route('/signup', methods = ['POST'])
+    @app.route('/signup', methods=['POST'])
     def signup():
         data = request.get_json()
         role = data.get('role')
-        username = data.get('username')
-        email = data.get('email')
-        phone_number = data.get('phone_number')
+        username = data.get('username').strip().lower()
+        email = data.get('email').strip().lower()
+        phone_number = data.get('phone_number').strip()
         password = data.get('password')
 
         existing_user = User.query.filter(
-            (User.username == username.strip().lower()) |
-            (User.email == email.strip().lower()) |
-            (User.phone_number == phone_number.strip())
+            (User.username == username) |
+            (User.email == email) |
+            (User.phone_number == phone_number)
         ).first()
 
         if existing_user:
-            if existing_user.username == username.strip().lower():
+            if existing_user.username == username:
                 return jsonify({'message': 'Username already exists'}), 400
-            elif existing_user.email == email.strip().lower():
+            elif existing_user.email == email:
                 return jsonify({'message': 'Email already exists'}), 400
-            else:
+            elif existing_user.phone_number == phone_number:
                 return jsonify({'message': 'Phone number already exists'}), 400
-        
+
         new_user = User(
-            role = role,
-            username = username,
-            email = email,
-            phone_number = phone_number,
-            password = bcrypt.generate_password_hash(password).decode('utf-8')
-    )
+            role=role,
+            username=username,
+            email=email,
+            phone_number=phone_number,
+            password=bcrypt.generate_password_hash(password).decode('utf-8')
+        )
 
         db.session.add(new_user)
         db.session.commit()
         return jsonify({'message': 'User created successfully'})
 
+    @app.route('/login', methods=['POST'])
+    def login():
+        data = request.get_json()
+        username_or_email = data.get('username_or_email')
+        password = data.get('password')
+
+        user = User.query.filter(
+            (User.username == username_or_email) | (User.email == username_or_email)
+        ).first()
+
+        if not user or not bcrypt.check_password_hash(user.password, password):
+            return jsonify({'message': 'Invalid credentials'}), 401
+
+        return jsonify({'message': f'Welcome {user.username}!'})
 
     return app
 
