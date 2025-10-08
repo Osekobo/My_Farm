c66107198d9e3e746e030a93d011626010a6993d
BarackAdida <adida.barack@student.moringaschool.com>
2025-10-08 23:02:16 +0300

    New route for CSS modif

--- the-farm/src/App.jsx
+++ the-farm/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
+import Expenses from "./components/Expenses"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />
+      {/* <Route path="/" element={<SignUp />} />
       <Route path="/signup" element={<SignUp />} />  
-      <Route path="/login" element={<Login />} />    
+      <Route path="/login" element={<Login />} />    */}
+      <Route path="/expenses" element={<Expenses/>}/>
     </Routes>
   );
 }
